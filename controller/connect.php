8a9e5505cbd494957e61f220892381ea5c045f0a
Tabay04 <38112095+Tabay04@users.noreply.github.com>
2021-01-12 14:46:43 +0700

    Update connect.php

--- controller/connect.php
+++ controller/connect.php
@@ -22,11 +22,11 @@
 // $conn = pg_connect("host=".$url." port=".$port." dbname=".$dbname." user=".$username." password=".$password) or die("Gagal");
 
 // koneksi RD
-$username='postgres';
-$password='12345';
-$url='localhost';
+$username='ewxxaxxnqcjefg';
+$password='3ad11041aa527cc30ea50773b49dfda2f228b47258ee86cad8e8c38c82d04924';
+$url='ec2-50-19-32-202.compute-1.amazonaws.com';
 $port=5432;
-$dbname='suratmenyuratRD';
+$dbname='d6nnfnifm1im0q';
 
 $conn = pg_connect("host=".$url." port=".$port." dbname=".$dbname." user=".$username." password=".$password) or die("Gagal");
 
